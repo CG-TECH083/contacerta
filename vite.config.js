79c79c6a9bdbc1d280d910a8e752c6d93546cc16
cgtech083 <cgtech083@gmail.com>
2026-08-29 16:53:24 -0300

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react' // ou o plugin que estiver usando
 
 export default defineConfig({
+  plugins: [react()],
   base: '/contacerta/',
-});
+})
